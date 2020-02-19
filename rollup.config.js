fa2fd6e2209ddaa1d6ec9f0e2b113c0f07ccefeb
ES <es019@protonmail.com>
2020-02-20 01:29:02 +0700

    Init.

---
+++ rollup.config.js
@@ -0,0 +1,15 @@
+import {terser} from "rollup-plugin-terser";
+import typescript from 'rollup-plugin-typescript2';
+
+module.exports = {
+  input: 'src/index.ts',
+  output: {
+    file: 'dist/index.js',
+    format: 'esm'
+  },
+  external: ['react'],
+  plugins: [
+    terser(),
+    typescript(),
+  ],
+};
