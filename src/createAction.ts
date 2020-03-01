033b576858abb2ef2d8d31ba8bd530bb66ffa0b7
ES <es019@protonmail.com>
2020-03-02 03:07:35 +0700

    Fix 'group' type for createAction

--- src/createAction.ts
+++ src/createAction.ts
@@ -1,18 +1,18 @@
-import {ChangerWithPayload, ActionWithPayload, Changer, Action} from './typings';
+import {ChangerWithPayload, ActionWithPayload, Action, Changer} from 'typings';
 
 export function createAction<S, G extends keyof S, P>({group, payload, changer}: {
-  group: keyof S;
+  group: G;
   payload: P;
   changer: ChangerWithPayload<S, G, P>;
 }): ActionWithPayload;
 
 export function createAction<S, G extends keyof S>({group, changer}: {
-  group: keyof S;
+  group: G;
   changer: Changer<S, G>;
 }): Action;
 
 export function createAction<S, G extends keyof S, P>({group, payload, changer}: {
-  group: keyof S;
+  group: G;
   payload?: P;
   changer: Changer<S, G> | ChangerWithPayload<S, G, P>;
 }) {
