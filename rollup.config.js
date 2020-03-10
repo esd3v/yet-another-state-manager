7f1a779dc986f9a4b0c790b77f2daf95d8545930
ES <es019@protonmail.com>
2020-03-11 05:28:25 +0700

    Replace bundlers with rollup-plugin-ts

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,17 @@
 import {terser} from "rollup-plugin-terser";
-import typescript from 'rollup-plugin-typescript2';
+import ts from "@wessberg/rollup-plugin-ts";
 
-module.exports = {
-  input: 'src/index.ts',
-  output: {
-    file: 'dist/index.js',
-    format: 'esm'
+export default [
+  {
+    input: 'src/index.ts',
+    output: {
+      file: 'dist/index.js',
+      format: 'esm'
+    },
+    external: ['react'],
+    plugins: [
+      terser(),
+      ts(),
+    ],
   },
-  external: ['react'],
-  plugins: [
-    terser(),
-    typescript({
-      tsconfig: "tsconfig.build.json",
-    }),
-  ],
-};
+];
--- src/useStore.tsx
+++ src/useStore.tsx
@@ -1,6 +1,6 @@
 import {useContext} from 'react';
 import {StoreContext} from './context';
-import {State, MappedContextActions, ContextActions} from 'typings';
+import {State, MappedContextActions, ContextActions} from './typings';
 
 export function useStore<T extends State, A extends MappedContextActions<ContextActions> = {}>() {
   const {state, actions, dispatch} = useContext(StoreContext);
